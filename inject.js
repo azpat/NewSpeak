4505ee05130444d13d1c44ae5a408434a5087657
AzPat <AzPat@hushmail.com>
2013-08-19 23:57:03 -0700

    Version 0.2 is complete:
    [X] Remove options and other stuff to get 0.1 done
    
    Version 0.2
    [x] Load dictionary from localStorage
    [x] Modify to use improved replacement algorithm.
    [x] Make,Add Logo for extensions page
    [x] Store dictionary into localStorage
      [x] fake terms
        [x] real terms
        [x] Load/Store display preference to localStorage (options page js)
        [x] Load default options if not set in localStorage
        [x] Options page html
        [x] Options page css : http://app.formassembly.com/theme-editor/#, http://designshack.net/articles/10-css-form-examples/
        [x] Options icon
        [x] Generalize the replacement text formatting - something like this:

--- inject.js
+++ inject.js
@@ -1,28 +1,69 @@
-//First get options  
-  chrome.extension.sendRequest({method: "getOptions"}, 
-    function(response){
-    console.log("inject:sendRequest: " + "getOptions="+response.NS.DisplayPreference);
-    NS=response.NS;
-    redPill(NS);
-    }
-  );
+console.log("RedPill returns newspeak to oldspeak.");
+var NS; //can NS be defined at this level?
 
-    // ----------------------------------------------------------
-    // This part of the script triggers when page is done loading
-    console.log("RedPill returns newspeak to oldspeak.");
-    // ----------------------------------------------------------
-function redPill(NS) {
-  var moksha = NS.Dictionary;
-  for (var p in moksha) {
-        //todo: http://blog.alexanderdickson.com/javascript-replacing-text
-        if (NS.DisplayPreference=='restore')
-        {
-          document.body.innerHTML = document.body.innerHTML.replace(RegExp(moksha[p].newspeak, 'ig'), '<span title="OldSpeak restored by OldSpeak for Chrome"><del>' + moksha[p].newspeak + '</del><ins style="font-style:italic;">' + moksha[p].oldspeak + '</ins></span>');
-        }
-        else {
-          document.body.innerHTML = document.body.innerHTML.replace(RegExp(moksha[p].newspeak, 'ig'), '<span title="NewSpeak revealed by NewSpeak for Chrome"><del>' + moksha[p].oldspeak + '</del><ins style="font-style:italic;">' + moksha[p].newspeak + '</ins></span>');
-        }
-        
+//First get options  
+chrome.extension.sendRequest({method: "getOptions"}, 
+  function(response){
+  console.log("inject:sendRequest: " + "getOptions="+response.NS.DisplayPreference);
+  NS=response.NS;  
+  walk(document.body);
   }
-};
-    
+);
+
+//This is the algorithm that Cloud-to-Butt uses, which seems to be better.
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+// Cloud to butt doesn't add HTML, to get this to work I had to use advice from here: http://james.padolsey.com/javascript/replacing-text-in-the-dom-its-not-that-simple/
+// TODO: use his new and improved library here: https://github.com/padolsey/findAndReplaceDOMText
+function handleText(textNode) 
+{
+  var temp = document.createElement('div');
+  var textReplaced = false;
+
+  for (var p in NS.Dictionary) {
+    if (RegExp(NS.Dictionary[p].newspeak, 'ig').test(textNode.nodeValue)) {
+      textReplaced=true;
+      if (NS.DisplayPreference=='restore') {
+        temp.innerHTML = textNode.nodeValue.replace(RegExp(NS.Dictionary[p].newspeak, 'ig'), '<span title="OldSpeak restored by NewSpeak for Chrome"><del>' + NS.Dictionary[p].newspeak + '</del><ins style="font-style:italic;text-decoration:none;"> ' + NS.Dictionary[p].oldspeak + '</ins></span>');
+      }
+      else {
+        temp.innerHTML = textNode.nodeValue.replace(RegExp(NS.Dictionary[p].newspeak, 'ig'), '<span title="NewSpeak revealed by NewSpeak for Chrome"><del>' + NS.Dictionary[p].oldspeak + '</del><ins style="font-style:italic;text-decoration:none;"> ' + NS.Dictionary[p].newspeak + '</ins></span>');
+      }
+    }
+  }
+  
+  if (textReplaced){
+    // Extract the newly produced nodes from temp element and insert them before the original textNode.
+    while (temp.firstChild) {
+      console.log(temp.firstChild);
+      (textNode.parentNode).insertBefore(temp.firstChild, textNode);
+    }
+    // remove this text node
+    textNode.parentNode.removeChild(textNode);
+  }
+}
